30b2ce19396da37761d45cb440df5897e4a96c83
frank-Mwangi <113598876+frank-Mwangi@users.noreply.github.com>
2023-01-20 20:58:10 +0300

    Beginner Ruby sample programs

---
+++ giraffe.rb
@@ -0,0 +1,4 @@
+name = "Kevin Wendell Crumb"
+age = "35"
+print ("There once was a man named " + name + ". He was " + age + " years old.")
+puts ("\n" + name[0, 7])
---
+++ user.rb
@@ -0,0 +1,5 @@
+puts "Enter your name: "
+name = gets.chomp()
+puts "Enter your age: "
+age = gets.chomp()
+puts ("Hello, " + name + ". You're so cool for a " + age + "-year old!") 
